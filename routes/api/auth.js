146669f5e391990d5dd3bb4396b2feb262ee5aae
kkbwk <rizalnov667@gmail.com>
2022-12-02 08:32:23 +0700

    add middleware for auth jwt token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,22 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 // @route   GET api/auth
 // @desc    test route
 // @access  public
-router.get("/", (req, res) => res.send("Auth Route"));
+
+// auth = middleware dari folder middleware
+router.get('/', auth, async (req, res) => {
+  try {
+    //req.user.id didapat dari proses verifikasi jwt di middleware
+    const user = await User.findById(req.user.id).select('-password'); // ('-password') artinya data user dari database akan ditampilkan tanpa data password
+    res.json(user);
+  } catch (err) {
+    console.err(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
