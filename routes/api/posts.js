72ccfb4126b2b34dffb0f67bed4f9890c9b13cd0
kkbwk <rizalnov667@gmail.com>
2023-02-18 00:40:43 +0700

    add post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,41 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+// models
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-// @route   GET api/posts
-// @desc    test route
-// @access  public
-router.get("/", (req, res) => res.send("Posts Route"));
+// @route   POST api/posts
+// @desc    Create a post
+// @access  private
+router.post(
+  '/',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        user: req.user.id,
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+      });
+
+      const post = await newPost.save();
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      res.status(400).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
