fc3c313a3f356f334990712722b33278a89185a4
kkbwk <rizalnov667@gmail.com>
2023-02-19 15:46:30 +0700

    add likes routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -33,7 +33,7 @@ router.post(
       res.json(post);
     } catch (error) {
       console.error(error.message);
-      res.status(400).send('Server Error');
+      res.status(500).send('Server Error');
     }
   }
 );
@@ -48,7 +48,7 @@ router.get('/', auth, async (req, res) => {
     res.json(posts);
   } catch (error) {
     console.error(error.message);
-    res.status(400).send('Server Error');
+    res.status(500).send('Server Error');
   }
 });
 
@@ -71,7 +71,7 @@ router.get('/:id', auth, async (req, res) => {
       return res.status(404).json({ msg: 'Post not found' });
     }
 
-    res.status(400).send('Server Error');
+    res.status(500).send('Server Error');
   }
 });
 
@@ -102,7 +102,62 @@ router.delete('/:id', auth, async (req, res) => {
       return res.status(404).json({ msg: 'Post not found' });
     }
 
-    res.status(400).send('Server Error');
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has already been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked' });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+    res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   PUT api/posts/unlike/:id
+// @desc    Unlike a post
+// @access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has already been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: 'Post has not yet been liked' });
+    }
+
+    // Get remove index
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+    res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
   }
 });
 
