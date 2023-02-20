b14a0b780f753fc9f2d6fdfebf213f6094c9aba3
kkbwk <rizalnov667@gmail.com>
2023-02-21 01:08:41 +0700

    add auth pages

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,111 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+// import axios from 'axios';
 
 const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const { name, email, password, confirmPassword } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    // check the password
+    if (password !== confirmPassword) {
+      console.log("Password don't match");
+    } else {
+      console.log('SUCCESS');
+      // const newUser = {
+      //   name,
+      //   email,
+      //   password,
+      // };
+      // try {
+      //   const config = {
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //     },
+      //   };
+      //   const body = JSON.stringify(newUser);
+      //   const res = await axios.post('/api/users', body, config);
+      //   console.log(res.data);
+      // } catch (error) {
+      //   console.error(error.response.data);
+      // }
+    }
+  };
+
+  return (
+    <Fragment>
+      <div className="container">
+        <h1 className="large text-primary">Sign Up</h1>
+        <p className="lead">
+          <i className="fas fa-user"></i> Create Your Account
+        </p>
+        <form className="form" onSubmit={(e) => onSubmit(e)}>
+          {/* name */}
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={(e) => onChange(e)}
+              required
+            />
+          </div>
+          {/* email */}
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={(e) => onChange(e)}
+            />
+            <small className="form-text">
+              This site uses Gravatar so if you want a profile image, use a
+              Gravatar email
+            </small>
+          </div>
+          {/* password */}
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={(e) => onChange(e)}
+              minLength="6"
+            />
+          </div>
+          {/* confirmPassword */}
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => onChange(e)}
+              minLength="6"
+            />
+          </div>
+          {/* submitButton */}
+          <input type="submit" className="btn btn-primary" value="Register" />
+        </form>
+        <p className="my-1">
+          Already have an account? <Link to="/login">Sign In</Link>
+        </p>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Register;
--- routes/api/users.js
+++ routes/api/users.js
@@ -15,7 +15,7 @@ router.post(
   // validasi inputan
   [
     check('name', 'Name is Required').not().isEmpty(),
-    check('email', 'Pelase include a valid email').isEmail(),
+    check('email', 'Please include a valid email').isEmail(),
     check(
       'password',
       'Please enter a password with 6 or more characters'
