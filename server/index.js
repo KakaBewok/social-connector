b566b3d44a83b48ac7178c6dd5b7130a8f0bf841
kkbwk <rizalnov667@gmail.com>
2023-03-14 00:10:35 +0700

    build

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const connectDB = require('./config/db');
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+// connect database
+connectDB();
+
+// init middleware
+app.use(express.json({ extended: false }));
+
+// mendefiniskan rute (ngambil route dari file yang ada di folder require)
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
