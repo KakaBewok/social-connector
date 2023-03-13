59731ceb643aae2b6402ff19a27de38e17998e65
kkbwk <rizalnov667@gmail.com>
2023-03-13 23:14:36 +0700

    build

--- server/server.js
+++ server/server.js
@@ -15,6 +15,6 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
